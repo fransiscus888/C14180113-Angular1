bc213a75392252eecaaf82cddd89eaec570a2013
fransiscus888 <c14180113@john.petra.ac.id>
2021-02-23 20:02:22 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,18 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  player=1
-  baris:number
-  kolom:number
-  board:String[]=["*****","*****","*****","*****","*****"]
+export class AppComponent {
+  player = 1;
+  baris: number;
+  kolom: number;
+  board: String[] = ["*****", "*****", "*****", "*****", "*****"];
+  tes: number[][] = [[1, 2, 3], [4, 5, 6]];
 
-  play(){
-    
-
-
-
-
+  play() {
+    this.board[0][0] = "a";
   }
-
 }
